fa20128ba3588010f45cff0ef2950a3e65d6d459
Shakil <shakil.ahmed2@northsouth.edu>
2024-11-20 00:23:12 +0600

    .

--- Dashboard/db2.sql
+++ Dashboard/db2.sql
@@ -3,21 +3,10 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `bdportal`
---
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `citizen`
---
+
 
 CREATE TABLE `citizen` (
   `CitizenID` int(12) NOT NULL,
@@ -393,7 +382,3 @@ ALTER TABLE `servicerequest`
   ADD CONSTRAINT `servicerequest_ibfk_1` FOREIGN KEY (`CitizenID`) REFERENCES `citizen` (`CitizenID`),
   ADD CONSTRAINT `servicerequest_ibfk_2` FOREIGN KEY (`ServiceID`) REFERENCES `services` (`ServiceID`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
