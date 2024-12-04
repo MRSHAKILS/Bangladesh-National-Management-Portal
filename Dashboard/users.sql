f4643eec0a58970cede9dbc6042a02479b22cee1
Shakil <shakil.ahmed2@northsouth.edu>
2024-12-04 11:24:07 +0600

    ssql part cleaned

--- Dashboard/users.sql
+++
@@ -1,7 +0,0 @@
-create table users (
-	user_id int primary key AUTO_INCREMENT,
-	username varchar(255) NOT NULL UNIQUE,
-	email varchar(150) NOT NULL,
-	password varchar(255) NOT NULL,
-	registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
